ddf802e9f8ecd1f4431d4c2b883d064dfd2fd872
Isaac Weaver <therealgrbow@gmail.com>
2024-11-26 22:47:41 -0500

    trying js

---
+++ src/app/cassie/page.js
@@ -0,0 +1,164 @@
+import Image from "next/image";
+
+export const metadata = {
+  title: "TheRealGRBow | Cassie",
+  description: "a page dedicated to my girlfriend, Cassie.",
+};
+
+export default function Cassie() {
+  return (
+    <article className="m-0 p-3 pt-20 bg-gradient-to-br from-pink-600 h-full">
+      <section
+        className="pt-6 drop-shadow-[1px_1px_var(--tw-shadow-color)] shadow-black"
+        id="cassie"
+      >
+        <p className="text-2xl flex justify-center">
+          I LOVE MY GIRLFRIEND &#60;3
+        </p>
+        <p className="pt-3">
+          Hi Cassie, this is your 3 month anniversery present! I'm putting it
+          together while on the call with you, cause #1, I dont wanna leave you,
+          and #2, I love you.
+        </p>
+        <p className="pt-3">
+          Babygirl, you mean the fucking world to me, I never want another
+          moment in life without you. Just everything about you screams perfect
+          to me. We have a lot of good times, and a few bad times every once in
+          a while, but even during the rough rough shit, I would never want to
+          spend a moment without you. I will always stay, I will always love
+          you, throughout everything good and everything bad. I am going to be
+          here for you.
+        </p>
+        <p className="pt-3">
+          From the moment we started talking again back in July, to this moment
+          right now, I have been head over mother fucking heels in love with
+          you. I always wanted to be with you, and finally I got my girl, my
+          girlfriend, you, on September 1st, 2024. I will always remember that
+          date, well, I sure would hope so since like, its an easy to remember
+          date, plus it's our anniversery lol. It will always hold a special
+          place in my heart, the date I met my future wife. God I love you
+          Cassie.
+        </p>
+        <p className="pt-3">
+          Ya know, I don't think I hate a single thing about you, I think I love
+          everything. Everything about you is perfect, from the strands of hair
+          on your head, to the tipity tips of your toes. I love everything about
+          you Cassie. Even your heart is good, at least from what I've seen. You
+          care, you care aobut me, about your friends, about people in your
+          life, even people you haven't met, and you'll say its BS, but you do
+          care. It feels so good to have a beautiful girlfriend, a loving
+          girlfriend, and someone I want to be with forever.
+        </p>
+        <p className="pt-3">
+          Our happiest moments range from the dullest shit like playing Cookie
+          Clicker, to random ass parkour maps on Minecraft, to even just
+          laughing at YouTube videos. God I love the boring and the fun moments
+          with you. Every moment with you is just as fulfilling and entertaining
+          as the last. Every moment with you is worth spending, every moment
+          with you is a moment that I want to be closer to you and with you even
+          more than the last, because baby, I want you, and need you so bad. I'd
+          rather nothing more than to just sit there and call you all day, even
+          if we're doing nothing, even if we're driving each other crazy, that's
+          all I want to do, be with you.
+        </p>
+        <p className="pt-3">
+          Also, like, the level of care, and love I have for you is absolutely
+          insane bro. I want to be with you and give you a hug and a kiss every
+          time you get sad, every time you get mad, or even in our happiest
+          moments, I just wanna hug you and hold you. I wish in your saddest
+          moments that I could just wave a magic wand and make you feel better.
+          I mean I do what I can from across the country, but without actually
+          being with you, I feel useless. I care about every emotion you feel, I
+          want you to feel you can talk to me about anything however stupid you
+          might think it may be. I want our relationship to be built on that
+          openness and understanding baby. I love you so much. And like, if
+          anything ever was wrong, I want you to know I would literally rob a
+          bank to have enough money to be with you, to take care of you, to give
+          you the entire world if you were dying, make you feel better, and
+          nurse you back to health.
+        </p>
+        <p className="pt-3">
+          I can't wait to meet you and see you for the first time in person, God
+          that'll be a special day. A day where I get to hug you, kiss you, hold
+          your hand, and just hold you. A day where I can make you feel like the
+          only woman in the world, it'll be a day that will be special for the
+          rest of our lives, and I NEED it to come soon. I need to be able to
+          sleep with you, I need to be able to cuddle you, I need to be able to
+          remind you that you are my only reason for living, I need to remind
+          you that you are my only love, and the last love I'll ever have.
+        </p>
+        <p className="pt-3">
+          yo bitch, I feel like I've been yapping for so long.
+        </p>
+        <p className="pt-10">are you even still reading</p>
+        <p className="pt-10">do you even care what I'm like typing here</p>
+        <p className="pt-10">just a bit annoying</p>
+        <p className="pt-10">why the hell are you still here</p>
+        <p className="pt-10">you should like idk</p>
+        <p className="pt-10">stop reading bruh</p>
+        <p className="pt-10">nothing else will be typed here</p>
+        <p className="pt-10">yet you're still reading</p>
+        <p className="pt-10">dumb puppy</p>
+        <p className="pt-10">listen to me</p>
+        <p className="pt-10">stop fucking reading</p>
+        <p className="pt-10">get off this website</p>
+        <p className="pt-10">go to sleep or something</p>
+        <p className="pt-10">drink some water</p>
+        <p className="pt-10">get a snack</p>
+        <p className="pt-10">are you still reading?</p>
+        <p className="pt-10">god damn you stubborn puppy</p>
+        <p className="pt-10">fine I'll say some more stuff</p>
+        <p className="pt-10">maybe</p>
+        <p className="pt-10">idk</p>
+        <p className="pt-10">should I?</p>
+        <p className="pt-10">fine</p>
+        <p className="pt-10">whatever</p>
+        <p className="pt-10">you still here?</p>
+        <p className="pt-10">cool</p>
+        <p className="pt-10">anyways</p>
+        <p className="pt-10">sup cutie</p>
+        <p className="pt-10">sup pretty girl</p>
+        <p className="pt-10">sup my everything</p>
+        <p className="pt-10">sup puppy</p>
+        <p className="pt-10">sup cassie</p>
+        <p className="pt-10">sup princess</p>
+        <p className="pt-10">sup baby</p>
+        <p className="pt-10">sup babygirl</p>
+        <p className="pt-10">sup silly girl</p>
+        <p className="pt-10">sup puppy</p>
+        <p className="pt-10">Thank you so much for reading this far.</p>
+        <p className="pt-3">
+          Thank you so much for being my girlfriend for this long, I will always
+          love you, I will always care about you, and I promise to always tell
+          you the truth, no matter what. I promise to never leave you. I promise
+          to give you my everything for the rest of our days. I promise to
+          always fight for us, and to always give you kisses before bed. Thank
+          you for being mine, thank you for being my girlfriend. I never wanna
+          be without you ever again Cassie. Thank you for making me feel like
+          the most special boyfriend in the world. Thank you for making me feel
+          loved. Thank you for supporting me even when I do dumb shit. And thank
+          you for giving me so many chances, when God knows, I don't deserve
+          them. Thank you. I love you so much Cassie. Hope you like your
+          present.
+        </p>
+        <div className="pt-3">
+          <p>
+            Also, enjoy one of my favorite pictures of you, cause I love you.
+            You little perfect you. Look at you looking so pretty with the last
+            gift I got you. I love you so much
+          </p>
+          <figure>
+            <Image
+              className="pointer-events-none"
+              draggable="false"
+              src="/images/cassie.svg"
+              alt="her"
+              height="200"
+              width="200"
+            />
+          </figure>
+        </div>
+      </section>
+    </article>
+  );
+}
